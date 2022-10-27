977682dbd6f391f2acc43b64c5482339f168b0f8
Romain Thibaud <romainthibaud45@gmail.com>
2022-10-27 15:15:40 +0200

    add private route for ressource

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Home from './components/Home'
+import PrivateRoute from './components/PrivateRoute'
 
 import Resources from './components/Resources'
 
 import './i18n/i18n'
 import Error404Page from './components/404'
+import Error403Page from './components/403'
 
 export default function App() {
   return (
@@ -17,8 +19,16 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/resource" element={<Resources />} />
-        <Route path='*' element={<Error404Page />}/>
+        <Route
+          path="/resource"
+          element={
+            <PrivateRoute>
+              <Resources />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Error404Page />} />
+        <Route path="/403" element={<Error403Page />} />
       </Routes>
 
       <Footer />
